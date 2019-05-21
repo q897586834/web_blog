eead551428b5085523481c151ac0bc95ba521aa9
q897586834 <897586834@qq.com>
2019-05-21 16:31:25 +0800

    路由搭建完成

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -28,24 +28,30 @@ const getDetail = (id) => {
 
 }
 
-const newBlog =(blogData)=>{
+const newBlog = (blogData = {}) => {
     //blogData 是一个博客对象，包含 titile content 属性
     console.log(blogData)
-    return{
-        id:3//新建博客，插入到数据表里面的 id
-        
+    return {
+        id: 3 //新建博客，插入到数据表里面的 id
+
     }
 }
 
-const updateBlog = (id,blogData)=>{
+const updateBlog = (id, blogData = {}) => {
     //id 就是要更新博客的id
     //blogData 是一个博客对象，包含 titile content 属性
-    console.log('updatablog.....'+blogData.title+blogData.content)
+    console.log('updatablog.....' + blogData.title + blogData.content)
+    return true
+}
+
+const delBlog = (id) => {
+    //id 就是要删除博客的id
     return true
 }
 module.exports = {
     getList,
     getDetail,
     newBlog,
-    updateBlog
+    updateBlog,
+    delBlog
 }
--- src/router/blog.js
+++ src/router/blog.js
@@ -1,5 +1,5 @@
 const {
-    getList,getDetail,newBlog,updateBlog
+    getList,getDetail,newBlog,updateBlog,delBlog
 } = require('../controller/blog')
 const {
     SuccessModel,
@@ -41,8 +41,12 @@ const handleBlogRouter = (req, res) => {
 
     //删除一篇博客
     if (method === 'POST' && req.path === '/api/blog/delete') {
-        return {
-            msg: '这是删除博客的接口'
+        const result = delBlog(id)
+        if(result){
+            return new SuccessModel()
+        }
+        else{
+            return new ErrorModel("删除博客失败")
         }
     }
 }
